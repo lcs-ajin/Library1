8264fb9740d169a7b93c0f27eca7e68ccca9c1ac
Anthony Jin <ajin@lcs.0n.ca>
2022-05-15 17:18:55 -0400

    Initial Commit

---
+++ Library1/Library1App.swift
@@ -0,0 +1,17 @@
+//
+//  Library1App.swift
+//  Library1
+//
+//  Created by Anthony Jin on 2022-05-15.
+//
+
+import SwiftUI
+
+@main
+struct Library1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
